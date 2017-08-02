7b90e3051ee09cc556869d15c716ed3c776b2f8d
ChloeColeongco <30606327+ChloeColeongco@users.noreply.github.com>
2017-08-02 13:30:23 -0600

    Create array_forEach_move_demo.js

---
+++ array_forEach_move_demo.js
@@ -0,0 +1,17 @@
+var items = [];
+
+function randNumBetween(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+var count = 5; 
+for (var i = -5; i < count; i++) { 
+    var item = Scene.createItem("LP_Car", i, 5, 0); 
+    item.setColor(randNumBetween(0, 255), randNumBetween(0, 255), randNumBetween(0, 255))
+    item.setScale(0.5);
+    items.push(item);
+}
+
+items.forEach(function(item) {
+    item.move(0, 10);
+})
