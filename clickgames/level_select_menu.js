04da3525a1d92aa51d5c448e057ef1ca189b90ac
ChloeColeongco <30606327+ChloeColeongco@users.noreply.github.com>
2017-08-03 12:42:43 -0600

    added comments

--- clickgames/level_select_menu.js
+++ clickgames/level_select_menu.js
@@ -1,3 +1,6 @@
+//see space: https://cospac.es/SBnH
+//navigate to second scene with the toggle at the bottom of the screen
+
 /*
 player object
 to be able to call faceTo() on animals
@@ -20,37 +23,37 @@ scene: click environment > copy id in top right corner of pop-up
 */
 
 //level 1 (Cubes)
-var butterfly = Scene.getItem("a9aw1huKWR");
-butterfly.faceTo(player.item);
+var butterfly = Scene.getItem("a9aw1huKWR"); //id for item placed directly in scene, can use any item from CoSpaces library
+butterfly.faceTo(player.item); //undocumented function, may change in the future
 butterfly.onActivate(function() {
-    Space.goTo("hHhYfXXkol4HiDKwYj0TlT");
+    Space.goTo("hHhYfXXkol4HiDKwYj0TlT"); //unique scene id
 });
 
 //level 2 - TBA
-var rabbit = Scene.getItem("N5ePiApg8e");
-rabbit.faceTo(player.item);
+var rabbit = Scene.getItem("N5ePiApg8e"); //id for item placed directly in scene, can use any item from CoSpaces library
+rabbit.faceTo(player.item); //undocumented function, may change in the future
 rabbit.onActivate(function() {
     rabbit.say("Level 2");
 });
 
 //level 3 - Just Play (Lions and Birds)
-var dog = Scene.getItem("JdxlTHDl2C");
-dog.faceTo(player.item);
+var dog = Scene.getItem("JdxlTHDl2C"); //id for item placed directly in scene, can use any item from CoSpaces library
+dog.faceTo(player.item); //undocumented function, may change in the future
 dog.onActivate(function() {
-    Space.goTo("3tEosaTcOV4ztf4X3l0LTy");
+    Space.goTo("3tEosaTcOV4ztf4X3l0LTy"); //unique scene id
 });
 
 //level 4 - Click Avoid (Elephant Herd)
-var horse = Scene.getItem("MJxslljugV");
-horse.faceTo(player.item);
+var horse = Scene.getItem("MJxslljugV"); //id for item placed directly in scene, can use any item from CoSpaces library
+horse.faceTo(player.item); //undocumented function, may change in the future
 horse.onActivate(function() {
-    Space.goTo("IjClgirjrsZ2cdylLtF2iF");
+    Space.goTo("IjClgirjrsZ2cdylLtF2iF"); //unique scene id
 });
 
 //level 5 - Shoot - TBA
 //encountering issues with being able to shoot multiple fireballs while spawning cubes in a scheduleRepeating function
-var elephant = Scene.getItem("jTak57HjNC");
-elephant.faceTo(player.item);
+var elephant = Scene.getItem("jTak57HjNC"); //id for item placed directly in scene, can use any item from CoSpaces library
+elephant.faceTo(player.item); //undocumented function, may change in the future
 elephant.onActivate(function() {
     elephant.say("Level 5");
 });
@@ -58,11 +61,11 @@ elephant.onActivate(function() {
 //back to menu button
 var back = Scene.getItem("Back to Menu");
   back.onActivate(function() {
-      Space.goTo("D1jcKhBNIfhV9S4wVLNHtw");
+      Space.goTo("D1jcKhBNIfhV9S4wVLNHtw"); //unique scene id
   });
 
 //play button
 var gameName = Scene.getItem('Play');
 gameName.onActivate(function() {
-    Space.goTo("3tEosaTcOV4ztf4X3l0LTy");
+    Space.goTo("3tEosaTcOV4ztf4X3l0LTy"); //unique scene id
 });
