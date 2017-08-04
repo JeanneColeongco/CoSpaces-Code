89f6111d9527617b08f76949747ec25ef3e917df
ChloeColeongco <30606327+ChloeColeongco@users.noreply.github.com>
2017-08-04 10:31:14 -0600

    commented code

--- demo/bare_bones_demo.js
+++ demo/bare_bones_demo.js
@@ -1,6 +1,11 @@
-var item = Scene.createItem("LP_Car", 0, 5, 0);
-item.setScale(0.5);
-item.setColor(255, 0, 0);
+//creates car item five units on the y axis (forward from default camera view)
+var item = Scene.createItem("LP_Car", 0, 5, 0); 
+
+item.setScale(0.5); //resize the item to half the default size
+item.setColor(255, 0, 0); //set the color to red - parameters are (r, g, b)
+
+//if item is clicked...
 item.onActivate(function() {
+    //...move the item 2 pixels along the y axis (forward from default camera view)
     item.move(0, 2);
 });
