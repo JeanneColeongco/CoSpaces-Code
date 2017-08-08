679d114ef7bb5915dcbd5342de47688b882956dc
ChloeColeongco <30606327+ChloeColeongco@users.noreply.github.com>
2017-08-08 13:56:32 -0600

    added comments

--- Chem-Lab-Simulations/Acid-Base-Magnesium-Reaction.js
+++ Chem-Lab-Simulations/Acid-Base-Magnesium-Reaction.js
@@ -1,13 +1,14 @@
 //see space: https://cospac.es/5wxD
 //navigate to scene 1 using the toggle at the bottom of the scene
+//click the beaker contents to start the reaction
 
 function delay() { //wrapping everything in a function for later use
 
-    //if called on, draws beaker
+    //if called on, draws beaker (tall, four-sided, translucent container)
     //could make it so it's parametered and can draw a beaker anywhere on the screen
     function beaker() {
-        var opacity = 0.5;
-        var width = 0.01;
+        var opacity = 0.5; //translucent (mimicking glass)
+        var width = 0.01; //very thin
         var height = 5;
         var length = 4;
 
@@ -22,38 +23,42 @@ function delay() { //wrapping everything in a function for later use
         side2.setWidth(width);
         side2.setHeight(height);
         side2.setLength(length);
-        side2.setPositionAngle(2, 3, 0, 0, 0, 5, 1.5708); //90 degrees
+        side2.setPositionAngle(2, 3, 0, 0, 0, 5, 1.5708); //90 degree rotation from default position
 
         var side3 = Scene.createItem("Cuboid", 0, 0, 0);
         side3.setOpacity(opacity);
         side3.setWidth(width);
         side3.setHeight(height);
         side3.setLength(length);
-        side3.setPositionAngle(0, 1, 0, 0, 0, 5, 3.14159); //180 degrees
+        side3.setPositionAngle(0, 1, 0, 0, 0, 5, 3.14159); //180 degree rotation from default position
 
         var side4 = Scene.createItem("Cuboid", 0, 0, 0);
         side4.setOpacity(opacity);
         side4.setWidth(width);
         side4.setHeight(height);
         side4.setLength(length);
-        side4.setPositionAngle(-2, 3, 0, 0, 0, 5, 4.71239); //270 degrees
+        side4.setPositionAngle(-2, 3, 0, 0, 0, 5, 4.71239); //270 degree rotation from default position
     }
 
-    beaker();
+    beaker(); //calling beaker function (drawing beaker in designated space)
 
     //beaker contents, can be anything really
     var acid = Scene.createItem("Cuboid", 0, 3, 0);
     acid.setScale(3.8);
-    acid.setColor(255, 0, 0);
+    acid.setColor(255, 0, 0); //coloring "acid" red
 
     //if called, changes color of beaker contents using individual r, g, b (red, green, blue) values
     function changeColor() {
-        var color = 0;
+        var color = 0; //can use one variable since I want change to happen at a regular pace to the same end number for both g and b values, if end numbers are different, different variables will be needed
+        
+        //change the color of the red acid to light pink
         var makePink = Scene.scheduleRepeating(function() {
           color += 2;
           acid.setColor(255, color, color);
+            
+          //if desired end number for g and b values is reached...
           if (color === 200) {
-            makePink.dispose();
+            makePink.dispose(); //...stop incrementing those values
           }
         }, 0);
     }
@@ -63,9 +68,8 @@ function delay() { //wrapping everything in a function for later use
         return Math.random() * (max - min) + min;
     }
 
-    //tried to make bubbles scale up and down...
-    //so it looks like they're growing and shrinking...
-    //haven't worked it out yet.
+    //tried to make bubbles scale up and down so it looks like they're growing and shrinking...
+    //haven't worked it out properly yet.
 
     //var expand = [];
     //var shrink = [];
@@ -136,13 +140,12 @@ function delay() { //wrapping everything in a function for later use
             if (bubbles.length > 80) {
                 //makeBubble.dispose();
                 bubbles.forEach(function(bubble) {
-                    //bubble.deleteFromScene();
+                    bubble.deleteFromScene();
                     /*var scale = 0.01;
                     var shrink = Scene.scheduleRepeating(function() {
                                 bubble.setScale(scale);
                                 scale-=0.1;
                     }, 0);*/
-                    bubble.deleteFromScene();
                 } 
             }
             //Space.log(bubbles.length);
@@ -156,28 +159,35 @@ function delay() { //wrapping everything in a function for later use
         Scene.schedule(bubbler.stop, 2);
     }
 
-    //if beaker contents is clicked, a catalyst/enzyme/what-have-you is dropped in...
-    //and the appropriate functions are called to simulate the reaction
+    //if beaker contents is clicked... 
     acid.onActivate(function() {
-        var i = 0;
+        //a catalyst/enzyme/what-have-you is dropped in...
         var base = Scene.scheduleRepeating(function() {
-            var cube = Scene.createItem("Cuboid", 0, 3, 5);
-            cube.setColor(100, 100, 100);
-            cube.moveLinear(0, 3, 0, 1);
-            var pos = cube.getPosition();
+            var cube = Scene.createItem("Cuboid", 0, 3, 5); //cube starts above the beaker
+            cube.setColor(100, 100, 100); //color cube gray
+            cube.moveLinear(0, 3, 0, 1); //cube is slowly lowered down into the beaker
+            
+            //and the appropriate functions are called to simulate the reaction
             cube.onCollisionEnter(function() {
                 bubbleUp();
                 changeColor();
                 base.dispose();
                 cube.deleteFromScene();
+                
+                //makes bubbles
                 var makeBubble = Scene.scheduleRepeating(function() {
+                    //creates a bubble at a random position within the beaker walls
                     var bubble = Scene.createItem("LP_Sphere", randNumBetween(-1.8, 1.7), randNumBetween(1.3, 4.7), 3.5);
-                    bubble.setColor(255, 255, 255);
-                    bubbles.push(bubble);
+                    bubble.setColor(255, 255, 255); //color bubbles white
+                    bubbles.push(bubble); //adds each generated bubble into bubble array
+                    
+                    //if the length of bubbles array (i.e. number of generated bubbles) exceeds 80...
                     if (bubbles.length > 80) {
-                        makeBubble.dispose();
+                        makeBubble.dispose(); //stop making bubbles
                         bubbles.forEach(function(bubble) {
-                            bubble.deleteFromScene();
+                            bubble.deleteFromScene(); //delete bubbles from scene
+                            
+                            //remove bubbles from bubbles array
                             var index = bubbles.indexOf(bubble);
                             bubbles.splice(index, 1);
                         })
