93a15ac5a77204d7145fe49fb42bba9f284e41f0
ChloeColeongco <30606327+ChloeColeongco@users.noreply.github.com>
2017-08-10 09:29:07 -0600

    linked to space

--- clickgames/level_three_default.js
+++ clickgames/level_three_default.js
@@ -1,3 +1,6 @@
+//see space: https://cospac.es/SBnH
+//nagivate to scene 4 using the toggle at the bottom of the scene
+
 //clears the scene with the exception of the items with these ids
 //note that any items you create will have their own ids
 //double click on items then click the first icon in the drop down that pops up to access them
